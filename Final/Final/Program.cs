ee9254d1633ee4eaec3865406a2de91bb146df2c
Ashtonval <tennisjedi20xx@gmail.com>
2023-12-19 20:27:31 -0800

    done almost
    finished everything except for restarting the function once it has been completed

--- Final/Final/Program.cs
+++ Final/Final/Program.cs
@@ -12,22 +12,20 @@ class Challenges
     {
         Console.WriteLine("Hello there, my name is Ashton and today I have a variety of programs for you to choose from.");
         Console.WriteLine("Choose a function:\n 1: Sum \n 2: Convert \n 3: PlusOne \n 4: Power \n 5: AgeDays \n 6: triArea \n 7: lessThanOrEqualToZero \n 8: lessThan100 \n 9: EqualTo \n 10: GiveMeSomething \n 11: Reverse \n 12: howManySeconds \n 13: internalAnglesSum \n 14: nameString \n 15: AndYes \n 16: Points \n 17: perimeterOfRec \n 18: helloName \n 19: animals \n 20: footballPoints \n 21: monthName \n 22: findMinMax \n 23: GetAbsSum \n 24: calculateExponent \n 25: MultiplyByLength \n 26: HammingDistance");
-        Console.WriteLine("Please choose the number of the function that you would like to try");
+        Console.WriteLine("Please choose the number of the function that you would like to try.");
         int a1 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine(Function(a1));
+        Console.WriteLine(FunctionB(a1));
+        Console.WriteLine(FunctionS(a1));
+        Console.WriteLine(FunctionA(a1));
+        Console.WriteLine(FunctionC(a1));
+        Console.WriteLine(FunctionH(a1));
+        Console.WriteLine(FunctionM(a1));
 
 
 
-        /*Console.WriteLine("Enter a number");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter a number");
-        int a1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter a number");
-        int a2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter a number");
-        int a3 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter a number");
-        int a4 = Convert.ToInt32(Console.ReadLine());*/
+
+        
 
 
 
@@ -82,11 +80,11 @@ class Challenges
     {
         if (a <= 0)
         {
-            return false;
+            return true;
         }
         else
         {
-            return true;
+            return false;
         }
 
     }
@@ -210,12 +208,12 @@ class Challenges
     {
         int b = a.Min();
         int c = a.Max();
-        Console.WriteLine("The minimum value is " + b);
-        return (c);
+        Console.WriteLine("The minimum value is " + b + " and the maximum value is");
+        return c;
 
 
     }
-    public static int getAbsSum(int a, int b, int c, int d, int e)
+    public static int? getAbsSum(int a, int b, int c, int d, int e)
     {
         a = Math.Abs(a);
         b = Math.Abs(b);
@@ -230,7 +228,7 @@ class Challenges
         int z = (int)Math.Pow(a, b);
         return z;
     }
-    public static string MultiplyByLength(int[] a)
+    public static int? MultiplyByLength(int[] a)
     {
         for (int i = 0; i < a.Length; i++)
         {
@@ -251,7 +249,103 @@ class Challenges
         }
         return count;
     }
-    public static int Function(int a)
+    public static int? FunctionM(int a)
+    {
+        if (a == 25)
+        {
+            Console.WriteLine("This function returns the amount of letters that are in the phrase that the user inputs");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a6 = Convert.ToInt32(Console.ReadLine());
+            int[] z = { a1, a2, a3, a4, a5, a6 };
+            return MultiplyByLength(z);
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+    public static int? FunctionH(int a)
+    {
+        if(a == 26)
+        {
+            Console.WriteLine("This function gives the difference in the two phrases that the user inputs");
+            Console.WriteLine("Enter a phrase");
+            string a1 = Console.ReadLine();
+            Console.WriteLine("Enter a phrase");
+            string a2 = Console.ReadLine();
+            return HammingDistance(a1, a2);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public static int? FunctionA(int a)
+    {
+        if (a == 22)
+        {
+            Console.WriteLine("This function tells the user what the minimum and maximum values are of the array that they input values for");
+            Console.WriteLine("Enter a number");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a4 = Convert.ToInt32(Console.ReadLine());
+            int []c = {z, a1, a2, a3, a4 };
+            return FindMinMax(c);
+        }
+        else if(a == 23)
+        {
+            Console.WriteLine("This function gives the sum of all values that the user inputs.  All of the inputs are returned with their absolute values");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a5 = Convert.ToInt32(Console.ReadLine());
+            return getAbsSum(a1, a2, a3, a4, a5);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public static int? FunctionC(int a)
+    {
+        if(a == 24)
+        {
+            Console.WriteLine("This function lets the user pick a base(b) and an exponent(e)");
+            Console.WriteLine("Enter a base");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an exponent");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            return CalculateExponent(a1, a2);
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public static int? Function(int a)
     {
 
         if (a == 1)
@@ -288,92 +382,159 @@ class Challenges
         }
         else if (a == 5)
         {
-           
+            Console.WriteLine("This function returns the value of the amount of days in a certain amount of years");
+            Console.WriteLine("Enter a number that you would like to use for years");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            return AgeDays(a1);
         }
         else if (a == 6)
         {
-
-        }
-        else if (a == 7)
-        {
-
-        }
-        else if (a == 8)
-        {
-
-        }
-        else if (a == 9)
-        {
-
-        }
-        else if (a == 10)
-        {
-
-        }
-        else if (a == 11)
-        {
-
+            Console.WriteLine("This function returns the area of a triangle");
+            Console.WriteLine("Enter a number for the base of the triangle");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number for the height of the triangle");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            return triArea(a1, a2);
         }
         else if (a == 12)
         {
-
+            Console.WriteLine("This function returns how many secodns are in a certain amount of hours");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            return howManySeconds(a1);
         }
         else if (a == 13)
         {
-
+            Console.WriteLine("This function returns the sum of internal angles after the user inputs how many sides they want");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            return internalAnglesSum(a1);
         }
-        else if (a == 14)
+        else if (a == 16)
         {
-
-        }
-        else if (a == 15)
-        {
-
-        }
-        else if (a == 16
-            )
-        {
-
+            Console.WriteLine("This function returns the amount of points from a game.  The first type of points is worth two and the second type is worth 3");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            return points(a1, a2);
         }
         else if (a == 17)
         {
-
-        }
-        else if (a == 18)
-        {
-
+            Console.WriteLine("This function returns the perimeter of a rec");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            return perimeterOfRec(a1, a2);
         }
         else if (a == 19)
         {
-
+            Console.WriteLine("This returns the amount of animals in a farm.  The first amount of animals is the value times 2, the second is the value times 4, and the third is the value times 4");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number for the height of the triangle");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            return animals(a1, a2, a3);
         }
         else if (a == 20)
         {
+            Console.WriteLine("This function returns the amount of football points.  The first value is the value times 3, the second value is just that, and the third value is times 0");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+            return FootballPoints(a1, a2, a3);
+        }
+        
+        else
+        {
+            return null;
 
         }
-        else if (a == 21)
+    }
+    public static string FunctionS(int a)
+    {
+        if(a == 10)
         {
-
+            Console.WriteLine("This function returns whatever string you input and puts something before it");
+            Console.WriteLine("Enter a name or phrase");
+            string a1 = Console.ReadLine();
+            return GiveMeSomething(a1);
         }
-        else if (a == 22)
+        else if(a == 14)
         {
-
+            Console.WriteLine("This function returns the name that the user inputs and adds Edabit after it");
+            Console.WriteLine("Enter a name");
+            string a1 = Console.ReadLine();
+            return nameString(a1);
         }
-        else if (a == 23)
+        else if(a == 18)
         {
-
+            Console.WriteLine("This function tells the user hello after they input their name");
+            Console.WriteLine("Enter a name");
+            string a1 = Console.ReadLine();
+            return HelloName(a1);
         }
-        else if (a == 24)
+        else if(a == 21)
         {
-
+            Console.WriteLine("This function gives a month that corresponds to a number that they choose");
+            Console.WriteLine("Enter a number 1-12");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            return MonthName(a1);
         }
-        else if (a == 25)
+        
+        else
         {
-
+            return null;
         }
-        else if (a == 26)
+    }
+    public static bool? FunctionB(int a)
+    {
+        if (a == 7)
         {
-
+            Console.WriteLine("This function checks to see if your number is less than or equal to zero");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            return lessThanOrEqualToZero(a1);
+        }
+        else if (a == 8)
+        {
+            Console.WriteLine("This function lets the user know if the two integers are less than 100");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            return lessThan100(a1, a2);
+        }
+        else if (a == 9)
+        {
+            Console.WriteLine("This function lets the user know if both numbers that they choose are equal to each other");
+            Console.WriteLine("Enter a number");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            return IsEqual(a1, a2);
+        }
+        else if (a == 11)
+        {
+            Console.WriteLine("This function tells the user if the boolean that they input is true or false to true");
+            Console.WriteLine("Enter a boolean");
+            bool a1 = Convert.ToBoolean(Console.ReadLine());
+            return Reverse(a1);
+        }
+        else if (a == 15)
+        {
+            Console.WriteLine("This function tells the user if the two booleans that they input are true or false");
+            Console.WriteLine("Enter a boolean");
+            bool a1 = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Enter a boolean");
+            bool a2 = Convert.ToBoolean(Console.ReadLine());
+            return andYes(a1, a2);
         }
         else
         {
